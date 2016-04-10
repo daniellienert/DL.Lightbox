0993249f36c8582055c0625cd31821f4cbb4be1d
Daniel Lienert <daniel@lienert.cc>
2016-04-10 09:25:19 +0200

    TASK: Initial commit

---
+++ Resources/Private/grunt/uglify.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bootstrapLightbox: {
+        src: [
+            'bower_components/photoswipe/dist/photoswipe.js',
+            'bower_components/photoswipe/dist/photoswipe-ui-default.js',
+            'JavaScript/PhotoSwipe.js'
+        ],
+        dest: '../Public/JavaScript/BootstrapLightbox.min.js'
+    }
+}
